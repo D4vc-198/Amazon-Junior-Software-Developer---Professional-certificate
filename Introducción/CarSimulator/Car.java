fa8ca24497f5a6c0c0e36c6b337101631def7cd5
d4vc-198 <contactodiegoavc@gmail.com>
2025-09-01 17:08:20 -0600

    Lab: Simulador de Carro

--- Introducción/CarSimulator/Car.java
+++ Introducción/CarSimulator/Car.java
@@ -1,3 +1,10 @@
+/**
+ * La clase { Car } representa a un carro con atributos básicos
+ * como: Marca (make), Modelo (model), Color (color) y año (year) 
+ * 
+ * Método principal
+ * getCarDetails() - Retorna un string con los detalles del coche.
+ */
 public class Car {
     String make;
     String model;
--- Introducción/CarSimulator/Main.java
+++ Introducción/CarSimulator/Main.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
-        
+        Car myFirstCar = new Car();
+        Scanner scanner = new Scanner(System.in);
+
+        // Creamos un carro individual
+        myFirstCar.make = "Toyota";
+        myFirstCar.model = "Corolla";
+        myFirstCar.color = "Blue";
+        myFirstCar.year = 2020;
+        myFirstCar.getCarDetails();
+
+        // Creamos una coleccion de carros 
+        // donde el usuario llenara la información de cada carro
+        Car[] cars = new Car[5];
+
+        for (int index = 0; index < 5; index++) {
+            cars[index] = new Car();
+
+            System.out.println("Enter details for car" + (index + 1));
+
+            System.out.print("Make: ");
+            cars[index].make = scanner.nextLine();
+
+            System.out.print("Model: ");
+            cars[index].model = scanner.nextLine();
+
+            System.out.print("Color: ");
+            cars[index].color = scanner.nextLine();
+
+            System.out.print("Year: ");
+            cars[index].year = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        // Imprimimos la coleccion de carros
+        System.out.println("Your Car Collection");
+        for (int index = 0; index < 5; index++) {
+            String cardDetails = cars[index].getCarDetails();
+
+            System.out.println("--- Car " + (index + 1) + "---");
+            System.out.println(cardDetails);
+        }
     }
 }
