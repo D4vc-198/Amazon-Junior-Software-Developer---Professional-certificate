820a5c395c75fd80358795855dc5d5e72ec916b6
d4vc-198 <contactodiegoavc@gmail.com>
2025-09-12 22:48:55 -0600

    ShoppingCart class

--- Introducción/online-shopping-simulator/ShoppingCart.java
+++ Introducción/online-shopping-simulator/ShoppingCart.java
@@ -1,5 +1,15 @@
+/*
+ * Cuando se llama a addItem() para añadir un objeto electrónico o de ropa al carrito de compras
+ * el polimorfismo garantiza que se llame a la versión correcta de la informacion de la pantalla en
+ * funcion del tipo de objeto real.
+ */
 public class ShoppingCart {
     
+    /*
+     * Recibe un parametro de tipo Product (Electronics o Clothing)
+     * Muestra la información del producto pasado por parámetro
+     * y muestra un mensaje adicional indicando que se ha agregado el producto al carrito
+     */
     public void addItem(Product item) {
        item.displayInfo();
        
