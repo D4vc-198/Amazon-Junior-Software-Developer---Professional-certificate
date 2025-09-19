ca9476e7d08fb824583ebea0405395e96c30affb
d4vc-198 <contactodiegoavc@gmail.com>
2025-09-18 20:56:19 -0600

    Diseño lógico: Se adapto la clase espreso utilizando el polimorfiso para mostrar información especifica a la clase espresso

--- Introducción/CoffeMachine/Espresso.java
+++ Introducción/CoffeMachine/Espresso.java
@@ -1,8 +1,7 @@
 public class Espresso extends Coffee {
-    
+
     // Almacena el número de porciones
     int numberOfShots;
-
 
     public Espresso(String name, String roast, double price, int numberOfShots) {
         // Llamamos al constructor de la superClase
@@ -10,10 +9,29 @@ public class Espresso extends Coffee {
         this.numberOfShots = numberOfShots;
     }
 
-    public void printEspressoDetails() {
+    /*
+     * Sobrescribimos los métodos de la superClase Coffee para personalizar
+     * el comportamiento específico del Espresso. Aqui hacemos uso del polimorfismo
+     * para adaptar los métodos grindBeans, brewCoffee y printInfo a las necesidades
+     * del Espresso
+     */
+
+    @Override
+    public void grindBeans() {
+        System.out.println("Grinding espresso beans finely...");
+    }
+
+    @Override
+    public void brewCoffee() {
+        System.out.println("Preparing espresso at high pressure...");
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
         System.out.println("You ordered " + numberOfShots + " servings!");
         System.out.println("Each serving of espresso costs $" + super.price + ".");
-        System.out.println("Your total bill is: $" + (numberOfShots * super.price) );
+        System.out.println("Your total bill is: $" + (numberOfShots * super.price));
     }
 
 }
